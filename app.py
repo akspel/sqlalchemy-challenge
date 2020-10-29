a7b4d59504172bc57a9666c84557d927dc4a6aaf
Alexis Speliotis <akspeliotis@gmail.com>
2020-10-28 17:49:00 -0700

    Final created

--- app.py
+++ app.py
@@ -1,46 +1,4 @@
-# Practice
-## Step 2 - Climate App
-
-# Now that you have completed your initial analysis, design a Flask API based on the queries that you have just developed.
-
-# Use Flask to create your routes.
-
-### Routes
-
-#  `/`
-
-  # Home page.
-
-  # List all routes that are available.
-
-# `/api/v1.0/precipitation`
-
-  # Convert the query results to a dictionary using `date` as the key and `prcp` as the value.
-
-  # Return the JSON representation of your dictionary.
-
-# `/api/v1.0/stations`
-
-    # Return a JSON list of stations from the dataset.
-
-# `/api/v1.0/tobs`
-  # Query the dates and temperature observations of the most active station for the last year of data.
-  
-  # Return a JSON list of temperature observations (TOBS) for the previous year.
-
-# `/api/v1.0/<start>` and `/api/v1.0/<start>/<end>`
-
-  # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
-
-  # When given the start only, calculate `TMIN`, `TAVG`, and `TMAX` for all dates greater than and equal to the start date.
-
-  # When given the start and the end date, calculate the `TMIN`, `TAVG`, and `TMAX` for dates between the start and end date inclusive.
-
-## Hints
-
-# You will need to join the station and measurement tables for some of the queries.
-
-# Use Flask `jsonify` to convert your API data into a valid JSON response object.
+# app.py
 
 import numpy as np
 
@@ -101,10 +59,10 @@ def welcome():
     </html>
 
 
-@app.route("/api/v1.0/previpitation")
+@app.route("/api/v1.0/precipitation")
 def precipitation():
   # Docstring
-  """Retunr a list of previpitations from last year"""
+  """Return a list of previpitations from last year"""
   # Design a query to retrieve the last 12 months of precipitation data and plot the results 
   max_date = session.query(Measurement.date)order_by(Measurement.date.desc()).first()
 
